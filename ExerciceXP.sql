77d91d425e67a4ab4ef306cda9b0d4271d09eb31
SORO <kolosiaka@gmail.com>
2023-02-15 19:38:09 +0100

    'w8d1'

---
+++ ExerciceXP.sql
@@ -0,0 +1,4 @@
+--SELECT first_name,last_name,birth_day from students ;
+--SELECT first_name,last_name,birth_day from students ORDER BY last_name ASC LIMIT 4;
+--SELECT first_name,last_name,birth_day from students WHERE birth_day > '01/01/2000';
+--SELECT * from students  LIMIT 3 OFFSET 2 ;
